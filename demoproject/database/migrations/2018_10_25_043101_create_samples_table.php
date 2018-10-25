0851d60ad25860d89fe867ca16b08b688de0c9c0
Res Yeg <shaqk702@gmail.com>
2018-10-24 21:34:13 -0700

    create database layer

---
+++ demoproject/database/migrations/2018_10_25_043101_create_samples_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSamplesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('samples', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('UUID')->nullable();
+            $table->string('name')->nullable();
+            $table->string('description')->nullable();
+            $table->string('code')->nullable();
+            $table->string('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('samples');
+    }
+}
